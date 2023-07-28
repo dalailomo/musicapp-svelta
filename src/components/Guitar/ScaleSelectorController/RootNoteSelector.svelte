35277fe78502afdfaead15d4398f92d20e2e3768
peter-sanchez <92320441+peter-sanchez@users.noreply.github.com>
2023-07-28 10:28:37 +0200

    add root note selector instead of cringy input

--- src/components/Guitar/ScaleSelectorController/RootNoteSelector.svelte
+++ src/components/Guitar/ScaleSelectorController/RootNoteSelector.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
+	import { NOTES } from '$lib/consts';
 	import { createEventDispatcher } from 'svelte';
 	const dispatch = createEventDispatcher();
 
 	export let root: string;
+	const noteList: Note[] = NOTES;
 
 	function onRootInput(e: Event) {
 		dispatch('root-note-selected', (e.target as HTMLInputElement).value as Note);
 	}
 </script>
 
-<input type="text" on:input={onRootInput} bind:value={root} />
+<select bind:value={root} on:change={onRootInput}>
+	{#each noteList as noteListName}
+		<option value={noteListName}>
+			{noteListName}
+		</option>
+	{/each}
+</select>
+
